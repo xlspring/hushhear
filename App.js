8b77b575dbc58ed4de6a216ea7680bb021b2567b
xlspring <tsyomav@gmail.com>
2024-05-17 15:44:23 +0300

    Add login navigation

--- App.js
+++ App.js
@@ -1,16 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
+import {useEffect, useState} from "react";
 import { StyleSheet, Text, View } from 'react-native';
+
+import { StatusBar } from 'expo-status-bar';
+import * as NavigationBar from 'expo-navigation-bar';
 import { NavigationContainer } from "@react-navigation/native";
 
-import AppScreens from "./screens";
+import AppScreens from "./screens/app";
 import {SafeAreaProvider} from "react-native-safe-area-context";
+import LoginScreens from "./screens/login";
 
 export default function App() {
+  const [isLoggedIn, setLogIn] = useState(true);
+
+  useEffect(() => {
+    NavigationBar.setPositionAsync("absolute");
+    NavigationBar.setBackgroundColorAsync("#ffffff01");
+  }, []);
+
   return (
     <SafeAreaProvider>
       <NavigationContainer>
-        <AppScreens />
+        {
+          isLoggedIn ? (
+            <AppScreens />
+          ) : (
+            <LoginScreens />
+          )
+        }
       </NavigationContainer>
+      <StatusBar />
     </SafeAreaProvider>
   );
 }
---
+++ screens/login/accountChooser.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native';
+import {Button} from "react-native-paper";
+
+export default function AccountChooserScreen() {
+	return (
+		<View>
+			<Text>There you will choose an account</Text>
+		</View>
+	);
+}
